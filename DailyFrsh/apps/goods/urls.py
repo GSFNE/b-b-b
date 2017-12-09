0efc7359c7a2c64b5396d9cf9bcdfbf03514511d
GSF <123@163.com>
2017-12-09 09:48:56 +0800

    静态页面的数据的更新

--- DailyFrsh/apps/goods/urls.py
+++ DailyFrsh/apps/goods/urls.py
@@ -1,8 +1,8 @@
 from django.conf.urls import url
-from apps.goods.views import IndexView
+from apps.goods.views import IndexView, DetailView
 
 
 urlpatterns = [
     url(r'^index$', IndexView.as_view(), name='index'),  # 首页
-    url(r'', )
+    url(r'^goods/(?P<sku_id>\d+)$', DetailView.as_view(), name='detail')  # 商品详情页
 ]
--- DailyFrsh/apps/goods/views.py
+++ DailyFrsh/apps/goods/views.py
@@ -5,6 +5,8 @@ from apps.goods.models import GoodsType, IndexGoodsBanner, IndexPromotionBanner,
 from apps.order.models import OrderGoods
 from django.core.urlresolvers import reverse
 
+from django.core.cache import cache
+
 
 
 # Create your views here.
@@ -12,24 +14,37 @@ from django.core.urlresolvers import reverse
 class IndexView(View):
     # get方式,返回主页面
     def get(self, request):
-        # 从数据库读取数据
-        # 要读取的数据:
-        # 商品的全部分类,(左侧的6个分类);  中间滚动的banner图; 右侧的两张搞活动的图;
-        # 1. 商品的全部分类
-        types = GoodsType.objects.all()
-        # 2. 中间滚动图片
-        index_banner = IndexGoodsBanner.objects.all().order_by('index')
-        # 3. 右侧两张搞活动的图片
-        promotion_banner = IndexPromotionBanner.objects.all().order_by('index')
-        # 首页商品分类展示信息
-        for type in types:
-            # 查询首页展示的文字商品信息
-            title_banner = IndexTypeGoodsBanner.objects.filter(type=type, display_type=0).order_by('index')
-            # 查询首页展示的图片展示信息
-            image_banner = IndexTypeGoodsBanner.objects.filter(type=type, display_type=1).order_by('index')
-            type.title_banner = title_banner
-            type.image_banner = image_banner
+        # 尝试从缓存读取数据
+        context = cache.get('index_page_data')
+        if context is None:
+            # 设置缓存
+            # 从数据库读取数据
+            # 要读取的数据:
+            # 商品的全部分类,(左侧的6个分类);  中间滚动的banner图; 右侧的两张搞活动的图;
+            # 1. 商品的全部分类
+            types = GoodsType.objects.all()
+            # 2. 中间滚动图片
+            index_banner = IndexGoodsBanner.objects.all().order_by('index')
+            # 3. 右侧两张搞活动的图片
+            promotion_banner = IndexPromotionBanner.objects.all().order_by('index')
+            # 首页商品分类展示信息
+            for type in types:
+                # 查询首页展示的文字商品信息
+                title_banner = IndexTypeGoodsBanner.objects.filter(type=type, display_type=0).order_by('index')
+                # 查询首页展示的图片展示信息
+                image_banner = IndexTypeGoodsBanner.objects.filter(type=type, display_type=1).order_by('index')
+                type.title_banner = title_banner
+                type.image_banner = image_banner
 
+            context = {
+                'types': types,
+                'index_banner': index_banner,
+                'promotion_banner': promotion_banner
+            }
+
+            # 设置缓存,三个参数: 缓存的key(要通过这个key取出缓存), 上下文, 缓存有效期
+            cache.set('index_page_data', context, 3600)
+        # else:   这里省略了, 表示有缓存
         # 获取用户
         user = request.user
         # 默认的购物车的数量为0, 如果有加入收藏的商品,从数据库中读取,没有为0
@@ -41,18 +56,14 @@ class IndexView(View):
             # 这里保存收藏夹用的是hash  一个用户对应一个hash redis缓存
             # cart_1(goods_id: num, goods_id: num)/ cart_2(goods_id:num, good_id: num)/...
             # hlen  是哈希的一种方法,得到的是 键的个数
+        # 更新上下文, 购物车条目数
+        context.update(cart_count=cart_count)
 
-        context = {
-            'types': types,
-            'index_banner': index_banner,
-            'promotion_banner': promotion_banner,
-            'cart_count': cart_count,
-        }
         # 返回主页面
         return render(request, 'index.html', context)
 
 # 前端向后端传递参数的方式:
-# 1. url 参数传递 goods/list/1/1
+# 1. url 参数传递 ,正则匹配,匹配到要请求的路径
 # 2. get
 # 3. post
 
@@ -73,3 +84,35 @@ class DetailView(View):
         order_skus = OrderGoods.objects.filter(sku=sku).order_by('-create_time')
         # 3. 获取和商品同一个spu的其他规格商品(比如: 盒装草莓和500g草莓,商品一样,规格不一样)
         same_spu_skus = GoodsSKU.objects.filter(goods=sku.goods).exclude(id=sku.id)  # exclude()不包括自己
+        # 4. 获取购物车中的条目数,就是显示购物车内商品数量
+        # <1. 获取用户
+        user = request.user
+        cart_count = 0
+        if user.is_authenticated():
+            con = get_redis_connection('default')
+            cart_key = 'cart_%d' % user.id
+            cart_count = con.hlen(cart_key)  # 通过key取到key的长度
+
+            # 浏览记录是用户登陆才有的,用户不登陆是没有记录的,要写在if条件缩进里面
+            # 当用户访问详情页面的时候,应该添加客户浏览的历史记录, 用redis列表存储数据
+            con = get_redis_connection('default')
+            history_key = 'history_%d' % user.id
+            # 在客户访问浏览记录里已经有的商品时,应该先移除记录里的数据,然后在从最前面(左侧插入)插入该条商品的id
+            # 尝试移除数据, 如果列表没有该数据,不会报错
+            con.lrem(history_key, 0, sku_id)
+            # 添加新数据
+            con.lpush(history_key, sku_id)
+            # 保留客户 最新浏览的5个数据
+            con.ltrim(history_key, 0, 4)
+
+        # 组织上下文模板
+        context = {
+            'sku': sku,
+            'new_skus': new_skus,
+            'order_skus': order_skus,
+            'same_spu_skus': same_spu_skus,
+            'cart_count': cart_count
+        }
+
+        return render(request, 'detail.html', context)
+
